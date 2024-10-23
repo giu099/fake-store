d880da61289e18188a214af50441ccfb379aeec5
Giuliano <giuliano.nardi77@gmail.com>
2024-10-23 17:11:10 -0300

    modificacion generales, agregado de fondo y mas estilos

--- src/components/MainComponent.jsx
+++ src/components/MainComponent.jsx
@@ -2,15 +2,12 @@ import React from "react";
 import { useState,useEffect } from "react";
 import axios from "axios";
 import "../estilos/MainComponent.css";
-import SearchBar from "./SearchBar";
-
-
-
-
+import NavBar from "./NavBar";
 
 function MainComponent (){
     const [productos,setProductos] = useState([]);
     const [search, setSearch] = useState('')
+    const [productosFiltrados, setProductosFiltrados] = useState([])
     const [limit, setLimit] = useState(1)
     let baseURL = 'https://fakestoreapi.com/products?limit=1';
 
@@ -18,36 +15,50 @@ function MainComponent (){
         baseURL =  `https://fakestoreapi.com/products?limit=${limit}`
         axios.get(baseURL).then((res) => {
         setProductos(res.data);
-        
+        setProductosFiltrados(res.data);
         });
     },[limit]);
+
+    useEffect(() => {
+        const results = productos.filter((producto)=>{
+            return producto.title.includes(search);
+        })
+        setProductosFiltrados(results);
+    },[search,productos])
 
     const limitChange = (event) =>{
         setLimit(event.target.value);
     
     };
 
+    const searchChange = (event) => {
+        setSearch(event.target.value);
+    }
+
+
+
     return (
         <>
-            
-            <h1>Fakestore</h1> 
-            <h2>Ingrese la cantidad de productos que desea ver del 1 al 19</h2>
-            <input className='inputNumber' type="number" onChange={limitChange}/>
-            <div className="contenedorCards">
-                {productos.map((p)=> (
-                <>
-                    
-                    <div className='contenedorCatalogo'>
-                        <h3>{p.title}</h3>
-                        <img className='imgCatalogo' src={p.image} alt="" />
-                        <h3>El precio del producto es <br></br> {p.price}</h3>
-                        
-                    </div>
-                    
-                </>
-            ))}
+            <NavBar />
+            <div className="inicio">
+                <input className='inputNumber' type="number" onChange={limitChange} placeholder="Ingresa un numero de productos......"    />
+                <input className='inputSearch' type="text" placeholder="Buscar producto..." value={search} onChange={searchChange}/>
             </div>
-            <SearchBar/>
+            <section id="productos">    
+                <div className='mainContainer'>
+                    {productosFiltrados.length > 0 ? (
+                        productosFiltrados.map((product) => (
+                            <div className='contenedorCardsProducts'>
+                                <h3>{product.title}</h3>
+                                <p>${product.price}</p>
+                                <img src={product.image} alt={product.title} width="100" />
+                            </div>
+                        ))
+                    ) : (
+                        <h2>No se encontraron productos.</h2>
+                    )}
+                </div>
+            </section>
         </>
     );
 };
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,1 +1,21 @@
 import React, {useState} from "react";
+import "../estilos/NavBar.css"
+
+function NavBar ( ){
+    return (
+        <>
+            <nav>
+                <ul>
+                    <li><h1>FakeStore</h1></li>
+                    <div className="containList">    
+                        <li><a href="">Home</a></li>
+                        <li><a href="productos">Productos</a></li>
+                        <li><a href="">Contacto</a></li>
+                    </div>
+                </ul>
+            </nav>
+        </>
+    );
+};
+
+export default  NavBar;
